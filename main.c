47a9b8ba05e0074b13b9fd986b12c8c6274efbad
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-08-18 10:47:20 +0200

    Inicjalizacja okna Issue: MinilibX setup i inicjalizacja okna Fixes #4

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int main (void)
 {
-    printf("hello World\n");
-    printf("Tomek's testing\n");
+    void    *mlx_connection;
+    void    *mlx_window;
+
+    mlx_connection = mlx_init();
+    mlx_window = mlx_new_window(mlx_connection, 500, 500, "My window");
+    mlx_pixel_put(mlx_connection, mlx_window, 250, 250, 0xFFFFFF);
+    mlx_loop(mlx_connection);
 }
